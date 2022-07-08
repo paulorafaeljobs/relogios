a3a0ed66dc5717a5918af43d35eab6dda035d38e
paulorafaeljobs <paulo.rafael.jobs.@gmail.com>
2022-07-08 19:31:55 -0300

    deploy2

---
+++ js/produtos.js
@@ -0,0 +1,189 @@
+var produtos = [
+    {
+        id:1,
+        dir:'produto1',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Adidas Originals Masculino',
+        preço:869.00,
+        info:'O Relógio Adidas Originals Masculino ADH3081 tem sua caixa em aço inoxidável preto, pulseira de couro cinza  com acabamento refletivo, face interna da pulseira com acabamento emborrachado preto, visor em cristal mineral com mostrador cinza com acabamento refletivo. O Relógio Adidas Originals Masculino ADH3081 tem caixa com aproximadamente 46 mm de diâmetro e 10,5 mm de espessura, se caracterizando como um relógio grande. Pesa 98 g e tem resistência à água de 10 ATM (100 metros). Relógio analógico masculino com mecanismo de quartzo alimentado à bateria.<br> Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:2,
+        dir:'produto2',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Citizen Feminino',
+        preço:1049.00,
+        info:'Relógio analógico feminino com caixa e pulseira em aço inoxidável dourado, vidro de cristal mineral, 31.7 mm de diâmetro e pesando 67 g. Alimentação a bateria e com resistência de 3 atm.<br>Produto original com nota fiscal, manual e com 1 ano de garantia.'
+    },
+    {
+        id:3,
+        dir:'produto3',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Empório Armani Masculino Slim',
+        preço:1329.00,
+        info:'Clássico relógio Empório Armani AR11012, seu visor com detalhes em números romanos conferem elegåncia ao relógio que tem sua beleza realçada pelo tom azul da pulseira de couro e do fundo do visor, sem dúvida uma peça para arrematar com muita elegåncia o visual do homem moderno.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:4,
+        dir:'produto4',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Euro Feminino Fashion Fit Reflexos Prata',
+        preço:869.00,
+        info:'Modelo com Calendário, tem sua caixa em aço inoxidável prata, pulseira de aço inoxidável prata, visor em cristal mineral com mostrador azul espelhado. O Relógio Euro Feminino Fashion Fit Reflexos Prata EUJHS31BAA/3A tem caixa com aproximadamente 40 mm largura x 49,5 mm de altura e 10,5 mm de espessura, considerado em tamanho um relógio médio. Pesa 111 g e possui resistência à água de 5 ATM (50 metros).<br>Produto original com nota fiscal, manual e com 1 ano de garantia.'
+    },
+    {
+        id:5,
+        dir:'produto5',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Fossil Masculino Aeroflite',
+        preço:799.00,
+        info:'Caixa em aço inoxidável cinza, pulseira de couro genuíno preto, visor em cristal mineral  com mostrador cinza, marcadores e ponteiros com acabamentos fosforescentes que brilham no escuro. O Relógio Fóssil Masculino Aeroflite AM4552/0CN tem caixa com aproximadamente 44 mm de diâmetro e 13 mm de espessura, considerado em tamanho um relógio médio. Pesa 93 g e possui resistência à água de 10 ATM (100 metros). Relógio analógico masculino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:6,
+        dir:'produto6',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Invicta Masculino Pro Diver',
+        preço:869.00,
+        info:'Tem sua caixa e pulseira em aço inoxidável dourado, visor em Flame Fusion Crystal com mostrador azul. A caixa tem aproximadamente 48 mm de diâmetro e 16,5 mm de espessura. Pesa 312 g e tem resistência de 20 atm (200 metros). O seu mecanismo de quartzo com movimento suíço é alimentado à bateria.<br>Produto original com nota fiscal, manual e com 1 ano de garantia.'
+    },
+    {
+        id:7,
+        dir:'produto7',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Michael Kors Feminino Brinkley',
+        preço:2099.30,
+        info:'Relógio analógico feminino com Calendário, Cronógrafo e Hora 12/24, tem sua caixa em aço inoxidável prata, pulseira de aço inoxidável prata, visor em cristal mineral com mostrador prata, marcadores com brilhantes. O Relógio Michael Kors Feminino Brinkley MK6186 tem caixa com aproximadamente 40 mm de diâmetro e 12,5 mm de espessura, considerado em tamanho um relógio médio. Pesa 124 g e possui resistência à água de 5 ATM (50 metros). Relógio analógico feminino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:8,
+        dir:'produto8',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Michael Kors Feminino Channing MK6153',
+        preço:1679.00,
+        info:'Relógio analógico feminino com caixa em aço inoxidável dourado, pulseira de aço inoxidável dourado com acabamento em acetato alaranjado, visor em cristal mineral com mostrador dourado, marcadores em forma de algarismos romanos. O Relógio Michael Kors Feminino Channing MK6153 tem caixa com aproximadamente 41 mm de diâmetro e 9 mm de espessura, considerado em tamanho um relógio médio. Pesa 114 g e possui resistência à água de 10 ATM (100 metros). Relógio analógico feminino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:9,
+        dir:'produto9',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Michael Kors Feminino Hartman MK3490',
+        preço: 1699.00,
+        info:'Relógio analógico feminino com caixa em aço inoxidável dourado, pulseira de aço inoxidável dourado, visor em cristal mineral com mostrador dourado, marcadores em algarismos romanos. O Relógio Michael Kors Feminino Hartman MK3490 tem caixa com aproximadamente 39 mm de diâmetro e 7,5 mm de espessura, considerado em tamanho um relógio médio. Pesa 102 g e possui resistência à água de 5 ATM (50 metros). Relógio analógico feminino com mecanismo de quartzo alimentado à bateria.'
+    },
+    {
+        id:10,
+        dir:'produto10',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Michael Kors Feminino MK5916',
+        preço: 1309.00,
+        info:'Relógio analógico feminino com Calendário, Cronógrafo e Hora 12/24, tem sua caixa em aço inoxidável dourado, pulseira de aço inoxidável dourado, visor em cristal mineral  com mostrador branco, marcadores em forma de algarismos romanos. O Relógio Michael Kors Feminino MK5916 tem caixa com aproximadamente 44 mm de diâmetro e 12 mm de espessura, considerado em tamanho um relógio médio. Pesa 151 g e possui resistência à água de 10 ATM (100 metros). Relógio analógico feminino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:11,
+        dir:'produto11',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Michael Kors Feminino MK6594',
+        preço: 1789.00,
+        info:'Relógio analógico feminino com com Calendário e Hora 12/24, tem sua caixa em aço inoxidável vermelho com bisel e coroa em rose gold, pulseira de aço inoxidável vermelho, visor em cristal mineral com mostrador vemelho, marcadores e ponteiros rose gold, ponteiros com acabamentos fosforescentes que brilham no escuro. O Relógio Michael Kors Feminino MK6594 tem caixa com aproximadamente 40,5 mm largura x 44 mm de altura e 9 mm de espessura, considerado em tamanho um relógio médio. Pesa 90 g e possui resistência à água de 5 ATM (50 metros). Relógio analógico feminino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:12,
+        dir:'produto12',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Michael Kors Feminino Runway MK4285',
+        preço: 1699.00,
+        info:'Relógio analógico feminino com caixa em aço inoxidável dourado, pulseira de aço inoxidável dourado com acetato marfim, visor em cristal mineral  com mostrador marfim. O Relógio Michael Kors Feminino Runway MK4285 tem caixa com aproximadamente 44 mm de diâmetro e 9 mm de espessura, considerado em tamanho um relógio médio. Pesa 100 g e possui resistência à água de 5 ATM (50 metros). Relógio analógico feminino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:13,
+        dir:'produto13',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Michael Kors Feminino Runway MK8621',
+        preço: 1379.00,
+        info:'Relógio analógico feminino com caixa em aço inoxidável dourado, pulseira de aço inoxidável dourado, visor em cristal mineral com mostrador preto, marcadores e ponteiros dourados. O Relógio Michael Kors Feminino Runway MK8621 tem caixa com aproximadamente 44 mm de diâmetro e 9 mm de espessura, considerado em tamanho um relógio médio. Pesa 137 g e possui resistência à água de 5 ATM (50 metros). Relógio analógico feminino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:14,
+        dir:'produto14',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Orient Masculino MRSCC011 D2DX',
+        preço: 1269.00,
+        info:'O Relógio Masculino Orient MRSCC011 D2DX com Calendário e Cronógrafo, tem sua caixa em aço inoxidável azul e rose gold, pulseira de couro azul, visor em cristal mineral com mostrador azul, marcadores e ponteiros com acabamentos fosforescentes que brilham no escuro, caixa com aproximadamente 46 mm de diâmetro e 12 mm de espessura, considerado em tamanho um relógio grande, pesa 107 g e possui resistência à água de 10 ATM (100 metros). Relógio analógico masculino com mecanismo de quartzo alimentado à bateria.<br>Relógio Orient Original com nota fiscal, manual e com 1 ano de garantia.'
+    },
+    {
+        id:15,
+        dir:'produto15',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Skagen Feminino Ancher SKW2199',
+        preço: 1299.00,
+        info:'Skagen SKW2199, relógio analógico feminino com caixa e pulseira de aço inoxidável dourado, vidro de cristal mineral, 26 mm de diâmetro e pesando 65 g. Alimentação a bateria e com resistência de 3 atm.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:16,
+        dir:'produto16',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Skagen Masculino Holst SKW6237',
+        preço: 1039.00,
+        info:'Relógio masculino com caixa em aço inoxidável prata e pulseira de couro marrom, vidro de cristal mineral, 40 mm de diâmetro e pesando 32 g. Alimentação a bateria e com resistência de 5 atm.<br>Produto original com nota fiscal, manual e com 2 anos de garantia.'
+    },
+    {
+        id:17,
+        dir:'produto17',
+        img1:'img1.jpg',
+        img2:'img2.jpg',
+        img3:'img3.jpg',
+        img4:'img4.jpg',
+        nome:'Relógio Technos Masculino Performance Racer',
+        preço: 302.40,
+        info:'O modelo tem sua caixa em aço inoxidável preto, pulseira de aço inoxidável preto, visor em cristal mineral com mostrador preto, indexes e ponteiros prateados, ponteiros com acabamentos fosforescentes que brilham no escuro. O Relogio Technos Masculino Performance Racer 2035MNZ/4P tem caixa com aproximadamente 49 mm de largura x 54 mm de altura e 12 mm de espessura, se caracterizando como um relógio grande. Pesa 167 g e tem resistência à água de 5 ATM (50 metros). Relógio analógico masculino com mecanismo de quartzo alimentado à bateria.<br>Produto original com nota fiscal, manual e com 1 ano de garantia.'
+    }
+]
